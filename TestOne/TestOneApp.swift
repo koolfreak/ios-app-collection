798f10598d3d3f2fe045ae121abf61dda805bb44
Eman <1182352+koolfreak@users.noreply.github.com>
2023-01-23 16:46:44 +0800

    Initial Commit

---
+++ TestOne/TestOneApp.swift
@@ -0,0 +1,17 @@
+//
+//  TestOneApp.swift
+//  TestOne
+//
+//  Created by Emmanuel Nollase on 1/23/23.
+//
+
+import SwiftUI
+
+@main
+struct TestOneApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
